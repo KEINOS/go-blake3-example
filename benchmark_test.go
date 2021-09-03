bfc3fb4596ecffcc230652c2165ead8381e80ce8
KEINOS <github@keinos.com>
2021-09-03 15:20:25 +0900

    fix #1 more fair test and more tests (PR #2)
    close #1

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"hash"
+	"hash/adler32"
+	"hash/crc32"
 	"hash/fnv"
+	"io"
 	"testing"
 
+	"github.com/jzelinskie/whirlpool"
+	"github.com/reusee/mmh3"
+	blake3Zeebo "github.com/zeebo/blake3"
+	"golang.org/x/crypto/blake2b"
 	"golang.org/x/crypto/sha3"
-	"lukechampine.com/blake3"
+	blake3Luke "lukechampine.com/blake3"
 )
 
 // inputData holds 1MB(1e6) size of data created by testData() function.
@@ -15,84 +27,164 @@ var inputData []byte
 //  Tests
 // ============================================================================
 
-// ----------------------------------------------------------------------------
-//  FNV
-// ----------------------------------------------------------------------------
+func BenchmarkAdler_32b(b *testing.B) {
+	benchHashWriteAndSum(b, adler32.New())
+}
 
-func BenchmarkFNV1_4Byte(b *testing.B) {
+func BenchmarkBlake2b_256b(b *testing.B) {
+	h, err := blake2b.New256(nil)
+	if err != nil {
+		b.Fatal(err)
+	}
+	benchHashWriteAndSum(b, h)
+}
+
+func BenchmarkBlake2b_512b(b *testing.B) {
+	h, err := blake2b.New512(nil)
+	if err != nil {
+		b.Fatal(err)
+	}
+	benchHashWriteAndSum(b, h)
+}
+
+func BenchmarkBlake3_verLuke_256b(b *testing.B) {
+	h := blake3Luke.New(256, nil)
+	benchHashWriteAndSum(b, h)
+}
+
+func BenchmarkBlake3_verLuke_256b_Sum256(b *testing.B) {
 	data := testData(b)
 
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		h := fnv.New32()
-		_, _ = h.Write(data)
-		_ = h.Sum(nil)
+	for n := 0; n < b.N; n++ {
+		blake3Luke.Sum256(data)
 	}
 }
 
-func BenchmarkFNV1A_4Byte(b *testing.B) {
+func BenchmarkBlake3_verLuke_512b(b *testing.B) {
+	h := blake3Luke.New(512, nil)
+	benchHashWriteAndSum(b, h)
+}
+
+func BenchmarkBlake3_verLuke_512b_Sum512(b *testing.B) {
 	data := testData(b)
 
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		h := fnv.New32a()
-		_, _ = h.Write(data)
-		_ = h.Sum(nil)
+	for n := 0; n < b.N; n++ {
+		blake3Luke.Sum512(data)
 	}
 }
 
-// ----------------------------------------------------------------------------
-//  BLAKE3
-// ----------------------------------------------------------------------------
+func BenchmarkBlake3_verZeebo_256b(b *testing.B) {
+	benchHashWriteAndSum(b, blake3Zeebo.New())
+}
 
-func BenchmarkBlake3_32Byte(b *testing.B) {
+func BenchmarkBlake3_verZeebo_256b_Sum256(b *testing.B) {
 	data := testData(b)
 
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = blake3.Sum256(data)
+	for n := 0; n < b.N; n++ {
+		blake3Zeebo.Sum256(data)
 	}
 }
 
-func BenchmarkBlake3_64Byte(b *testing.B) {
+func BenchmarkBlake3_verZeebo_512b(b *testing.B) {
 	data := testData(b)
 
+	h := blake3Zeebo.New()
+
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = blake3.Sum512(data)
+
+	// To create longer or shorter than 32 byte zeebo needs
+	// to use Digest
+	for n := 0; n < b.N; n++ {
+		if _, err := h.Write([]byte(data)); err != nil {
+			b.Fatalf("failed to write data. Err: %v", err)
+		}
+
+		d := h.Digest()
+
+		out := make([]byte, 64)
+		d.Seek(0, io.SeekStart)
+		_, _ = d.Read(out)
 	}
 }
 
-// ----------------------------------------------------------------------------
-//  SHA3
-// ----------------------------------------------------------------------------
-
-func BenchmarkSHA3_32Byte(b *testing.B) {
+func BenchmarkBlake3_verZeebo_512b_Sum512(b *testing.B) {
 	data := testData(b)
 
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = sha3.Sum256([]byte(data))
+	for n := 0; n < b.N; n++ {
+		blake3Zeebo.Sum512(data)
 	}
 }
 
-func BenchmarkSHA3_64Byte(b *testing.B) {
-	data := testData(b)
+func BenchmarkMMH3_128b(b *testing.B) {
+	benchHashWriteAndSum(b, mmh3.New128())
+}
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = sha3.Sum512([]byte(data))
-	}
+func BenchmarkCRC_32b(b *testing.B) {
+	benchHashWriteAndSum(b, crc32.NewIEEE())
+}
+
+func BenchmarkFnv1_128b(b *testing.B) {
+	benchHashWriteAndSum(b, fnv.New128())
+}
+
+func BenchmarkFnv1a_128b(b *testing.B) {
+	benchHashWriteAndSum(b, fnv.New128a())
+}
+
+func BenchmarkMD5_128b(b *testing.B) {
+	benchHashWriteAndSum(b, md5.New())
+}
+
+func BenchmarkSHA1_160b(b *testing.B) {
+	benchHashWriteAndSum(b, sha1.New())
+}
+
+func BenchmarkSHA_256b(b *testing.B) {
+	benchHashWriteAndSum(b, sha256.New())
+}
+
+func BenchmarkSHA_512b(b *testing.B) {
+	benchHashWriteAndSum(b, sha512.New())
+}
+
+func BenchmarkSHA3_256b(b *testing.B) {
+	benchHashWriteAndSum(b, sha3.New256())
+}
+
+func BenchmarkSHA3_512b(b *testing.B) {
+	benchHashWriteAndSum(b, sha3.New512())
+}
+
+func BenchmarkWhirlpool_512b(b *testing.B) {
+	benchHashWriteAndSum(b, whirlpool.New())
 }
 
 // ============================================================================
 //  Helper Functions
 // ============================================================================
 
-// The testData creates 1000,000 bytes= 1MB(1e6) size of data.
+func benchHashWriteAndSum(b *testing.B, h hash.Hash) {
+	data := testData(b)
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		if _, err := h.Write(data); err != nil {
+			b.Fatalf("failed to write data. Err: %v", err)
+		}
+		_ = h.Sum(nil)
+	}
+}
+
+// The testData creates 1,000,000 bytes= 1MB (1e6) size of data.
+// The values are consistent and not random.
 func testData(b *testing.B) []byte {
 	b.Helper()
 
+	// Initialize data or use initialized data
 	if len(inputData) == 0 {
 		inputData = make([]byte, 1e6)
 
--- go.mod
+++ go.mod
@@ -3,12 +3,18 @@ module github.com/KEINOS/go-blake3-example
 go 1.17
 
 require (
+	github.com/jzelinskie/whirlpool v0.0.0-20201016144138-0675e54bb004
+	github.com/reusee/mmh3 v0.0.0-20140820141314-64b85163255b
+	github.com/stretchr/testify v1.7.0
+	github.com/zeebo/blake3 v0.2.0
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
+	lukechampine.com/blake3 v1.1.5
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/klauspost/cpuid v1.3.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
-	lukechampine.com/blake3 v1.1.5 // indirect
 )
